f6a3ae87cfae145a24e3d36d412d0eb46b22772b
Denis Vlassenko <vlassenko@adobe.com>
2023-03-30 14:59:19 +0200

    MWPW-127984 Load OST JS/CSS

--- libs/blocks/commerce/commerce.js
+++ libs/blocks/commerce/commerce.js
@@ -1,5 +1,29 @@
-import { createTag } from '../../utils/utils.js';
+import { createTag, loadScript } from '../../utils/utils.js';
 
 export default async function init(el) {
-  el.append(createTag('div', { class: 'container' }, 'OST will be hosted here'));
+  el.append(createTag('div', { class: 'ost' }));
+
+  if (!window.ost) {
+    await loadScript('https://www.stage.adobe.com/special/tacocat/ost/offer-selector-tool.js');
+  }
+
+  const ostCountry = 'US';
+  const ostLanguage = 'en';
+  const ostEnvironment = 'STAGE';
+  const ostWcsApiKey = 'wcms-commerce-ims-ro-user-cc';
+  const ostAosApiKey = 'dexter-commerce-offers';
+  const checkoutClientId = 'creative';
+  const ostAosAccessToken = sessionStorage.getItem('AOS_ACCESS_TOKEN') || localStorage.getItem('AOS_ACCESS_TOKEN');
+  const url = new URL(window.location);
+  const searchParameters = url.searchParams;
+  window.ost.openOfferSelectorTool({
+    ostCountry,
+    ostLanguage,
+    ostEnvironment,
+    ostWcsApiKey,
+    ostAosApiKey,
+    ostAosAccessToken,
+    checkoutClientId,
+    searchParameters,
+  });
 }
