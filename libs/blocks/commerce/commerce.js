3713b6480b313b968d55a2b6689708a9f0948da0
Denis Vlassenko <vlassenko@adobe.com>
2023-03-31 13:29:53 +0200

    Update OST urls, load its style

--- libs/blocks/commerce/commerce.js
+++ libs/blocks/commerce/commerce.js
@@ -1,29 +1,42 @@
-import { createTag, loadScript } from '../../utils/utils.js';
+import { createTag, getConfig, getLocale, loadStyle, loadScript } from '../../utils/utils.js';
+
+const config = getConfig();
+console.info('Config:', config);
+const params = new URLSearchParams(window.location.search);
+const referrer = params.get('referrer');
+const owner = params.get('owner');
+const repo = params.get('repo');
 
 export default async function init(el) {
   el.append(createTag('div', { class: 'ost' }));
 
   if (!window.ost) {
-    await loadScript('https://www.stage.adobe.com/special/tacocat/ost/offer-selector-tool.js');
+    const baseUrl = 'https://www.stage.adobe.com/special/tacocat/ost';
+    await loadScript(`${baseUrl}/index.js`);
+    loadStyle(`${baseUrl}/index.css`);
   }
 
-  const ostCountry = 'US';
   const ostLanguage = 'en';
-  const ostEnvironment = 'STAGE';
-  const ostWcsApiKey = 'wcms-commerce-ims-ro-user-cc';
-  const ostAosApiKey = 'dexter-commerce-offers';
-  const checkoutClientId = 'creative';
+  const ostCountry = 'US';
+  try {
+    const res = await fetch(`https://admin.hlx.page/status/${owner}/${repo}/main?editUrl=${referrer}`);
+    const json = await res.json();
+    console.info('Locale:', getLocale(config.locales, new URL(json.preview.url)));
+  } catch {
+    // ignore
+  }
+
+  // TODO: integrate with IMS
   const ostAosAccessToken = sessionStorage.getItem('AOS_ACCESS_TOKEN') || localStorage.getItem('AOS_ACCESS_TOKEN');
-  const url = new URL(window.location);
-  const searchParameters = url.searchParams;
+  // TODO: get price/checkout contexts from metadata.json and pass to OST
   window.ost.openOfferSelectorTool({
     ostCountry,
     ostLanguage,
-    ostEnvironment,
-    ostWcsApiKey,
-    ostAosApiKey,
+    ostEnvironment: 'STAGE',
+    ostWcsApiKey: 'wcms-commerce-ims-ro-user-cc',
+    ostAosApiKey: 'dexter-commerce-offers',
     ostAosAccessToken,
-    checkoutClientId,
-    searchParameters,
+    checkoutClientId: 'creative',
+    searchParameters: new URL(window.location).searchParams,
   });
 }
